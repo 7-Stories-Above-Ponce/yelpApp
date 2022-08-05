a1153984b1fa1b82f411eccc3069fd4285974332
Ihsan <muhammai2000@gmail.com>
2022-08-05 12:56:40 -0400

    first commit

---
+++ client/src/context/RestaurantsContext.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import React, {useState, createContext} from "react";
+
+export const RestaurantsContext = createContext();
+
+export const RestaurantsContextProvider = (props) => {
+    const [restaurants, setRestaurants] = useState([])
+    const [selectedRestaurant, setSelectedRestaurant] = useState(null);
+
+    const addRestaurants = (restaurants) => {
+        setRestaurants([...restaurants, restaurant]);
+    }
+    return (
+        <RestaurantsContext.Provider 
+        value={{
+            restaurants, 
+            setRestaurants, 
+            addRestaurants,
+            selectedRestaurant,
+            setSelectedRestaurant,
+            }}
+            >
+            {props.children}
+        </RestaurantsContext.Provider>
+    );   
+};
